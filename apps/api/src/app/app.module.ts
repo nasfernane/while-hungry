0a47a5e36f8f4b0867ea334a16f68077039132ba
Nassim Fernane <nasfernane@gmail.com>
2022-01-22 19:54:13 +0100

    [api - hotfix] fixed typo in app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { RecipesModule } from './../recipes/recipes.module'
-import { PostsModule } from './../modules/posts/posts.module'
+import { PostsModule } from './../posts/posts.module'
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
