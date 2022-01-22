d5c52d9e08daa90699254a6eea5dfc8da30b8f55
Nassim Fernane <nasfernane@gmail.com>
2022-01-22 16:20:14 +0100

    added app routing, core-data, core-state and material libs

--- apps/while-hungry/src/app/app.component.ts
+++ apps/while-hungry/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@wh/api-interfaces';
+import { Post } from '@wh/api-interfaces';
 
 @Component({
   selector: 'wh-root',
@@ -8,6 +8,6 @@ import { Message } from '@wh/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  hello$ = this.http.get<Post>('/api/hello');
   constructor(private http: HttpClient) {}
 }
--- apps/while-hungry/src/app/app.module.ts
+++ apps/while-hungry/src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from '@wh/material';
+import { CoreDataModule } from '@wh/core-data';
+import { CoreStateModule } from '@wh/core-state';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule, 
+    StoreModule.forRoot({}, {}), 
+    AppRoutingModule, 
+    MaterialModule,
+    CoreDataModule,
+    CoreStateModule,
+  ],
+    
   providers: [],
   bootstrap: [AppComponent],
 })
