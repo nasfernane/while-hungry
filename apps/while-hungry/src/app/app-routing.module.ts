b1b777a09d67b0ee55a3edc57d9d7db7ea1cbcc6
Nassim Fernane <nasfernane@gmail.com>
2022-02-25 09:27:29 +0100

    fixed missing register component in routing module

--- apps/while-hungry/src/app/app-routing.module.ts
+++ apps/while-hungry/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { BlogComponent } from './components/blog/blog.component';
 import { BlogNewPostComponent } from './components/blog/blog-new-post/blog-new-post.component';
 import { BlogPostComponent } from './components/blog/blog-post/blog-post.component';
 import { LoginComponent } from './components/auth/login/login.component';
-import { RegisterComponent } from './components/auth/register/register.component';
 import { RecipeComponent } from './components/recipes/recipe/recipe.component';
 
 import { AdminGuard  } from './guards/admin.guard';
@@ -16,7 +15,6 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'recipes', children: [
       { path: '', component: RecipesOverviewComponent },
       { path: ':id', component: RecipeComponent },
@@ -24,7 +22,7 @@ const routes: Routes = [
   },
   { path: 'blog', children: [
     { path: '', component: BlogComponent },
-    { path: 'post/new', component: BlogNewPostComponent, canActivate: [ AdminGuard ] }, 
+    { path: 'post/new', component: BlogNewPostComponent, canActivate: [ AuthGuard, AdminGuard ] }, 
     { path: 'post/:id', component: BlogPostComponent }
   ] 
 },
