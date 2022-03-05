fa94a97c07001ee9f02e50034edf464acc16ec49
Nassim Fernane <nasfernane@gmail.com>
2022-03-05 20:26:58 +0100

    update pipeline file

--- libs/core-data/src/lib/environments/environment.ts
+++ libs/core-data/src/lib/environments/environment.ts
@@ -1,10 +1,6 @@
 
-// export const environment = {
-//   production: false,
-//   API_URL: "//localhost:3000/api",
-// };
-
 export const environment = {
-  production: true,
-  API_URL: "//api.whilehungry.com",
+  production: false,
+  API_URL: "//localhost:3000/api",
 };
+
