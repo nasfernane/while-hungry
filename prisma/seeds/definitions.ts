bf451c0a04ef5ee00836bea6377d1b292d3889f6
Nassim Fernane <nasfernane@gmail.com>
2022-03-06 20:59:53 +0100

    update yml file

--- prisma/seeds/definitions.ts
+++ prisma/seeds/definitions.ts
@@ -47,7 +47,7 @@ export const definitions: Prisma.DefinitionCreateInput[] = [
     },
     {
         label: "Deglaze",
-        definition: "to remove and dissolve the browned food residue, or "glaze", from a pan to flavor sauces, soups, and gravies"
+        definition: "to remove and dissolve the browned food residue, or 'glaze', from a pan to flavor sauces, soups, and gravies"
     },
     {
         label: "Degrease",
@@ -102,23 +102,107 @@ export const definitions: Prisma.DefinitionCreateInput[] = [
         definition: "the process of softening or breaking into pieces using liquid, often referring to fruit or vegetables, in order to absorb the flavor of the liquid"
     },
     {
-        label: "",
-        definition: ""
+        label: "Mesclun",
+        definition: "a salad consisting of tender mixed greens such as lettuce, arugula, and chicory, herbs, and edible flowers"
     },
     {
-        label: "",
-        definition: ""
+        label: "Mince",
+        definition: "to finely divide food into uniform pieces smaller than diced or chopped foods, prepared using a chef's knife or food processor"
     },
     {
-        label: "",
-        definition: ""
+        label: "Nappe",
+        definition: "the ability of a liquid to coat the back of a spoon or the act of coating a food, such as a leg of lamb, with glaze"
     },
     {
-        label: "",
-        definition: ""
+        label: "Oeuf",
+        definition: "the French term for egg"
     },
     {
-        label: "",
-        definition: ""
+        label: "Parboiling ",
+        definition: "the process of adding foods to boiling waters, cooking until they are softened, then removing before they are fully cooked, usually to partially cook an item which will then be cooked another way"
+    },
+    {
+        label: "Pâté ",
+        definition: " a mixture of seasoned ground meat and fat minced into a spreadable paste"
+    },
+    {
+        label: "Persillade ",
+        definition: "a sauce or seasoning mixture of parsley chopped with seasonings, often used as part of a saute cook's mise en place  "
+    },
+    {
+        label: "Polenta",
+        definition: "a mush or porridge made from yellow or cornmeal which originated in Northern Italy"
+    },
+    {
+        label: "Quadriller ",
+        definition: "to make criss-cross lines on the surface of food, as part of food presentation"
+    },
+    {
+        label: "Quenelle",
+        definition: "a small quantity of a mixture of creamed fish or meat with a light egg binding, usually formed into a round shape, and then cooked"
+    },
+    {
+        label: "Remouillage",
+        definition: "a stock made from bones that have already been used once to make a stock, making it weaker"
+    },
+    {
+        label: "Rondeau ",
+        definition: "a wide, shallow pan with straight sides and two loop handles, often used for searing and poaching"
+    },
+    {
+        label: "Sear",
+        definition: "a technique used in grilling, baking, or sautéing in which the surface of the food is cooked at high temperature until a crust forms"
+    },
+    {
+        label: "Staling ",
+        definition: "a chemical and physical process in which foods such as bread become hard, musty, or dry, also known as 'going stale'"
+    },
+    {
+        label: "Steep",
+        definition: "to allow dry ingredients to soak in a liquid until the liquid takes on its flavor, often referring to coffee, tea, or spices"
+    },
+    {
+        label: "Sweat",
+        definition: "gently heating vegetables in a little oil, with frequent stirring and turning to ensure emitted liquid will evaporate; usually results in tender, or in some cases such as onions', translucent pieces"
+    },
+    {
+        label: "Tempering ",
+        definition: "raising the temperature of a cold or room-temperature ingredient by slowly adding hot or boiling liquid, often referring to eggs"
+    },
+    {
+        label: "Tourner ",
+        definition: "to cut ingredients such as carrots or potatoes into a barrel-like shape that form six or seven sides on the length of the item being cut, using a Tourner knife or a paring knife"
+    },
+    {
+        label: "Trussing",
+        definition: "to tie meat or poultry, such as turkey with a string, woven through the bird parts by using a needle, in order to create a more compact shape before cooking"
+    },
+    {
+        label: "Unleavened",
+        definition: "made without yeast or any other leavening agent, often referring to bread"
+    },
+    {
+        label: "Velouté ",
+        definition: "a type of sauce in which a light stock, such as chicken of fish, is thickened with a flour that is cooked and then allowed to turn light brown"
+    },
+    {
+        label: "Vol-au-Vent",
+        definition: "a round pastry that is baked and then filled with meat or vegetables after the fact"
+    },
+    {
+        label: "Whip",
+        definition: "to beat food with a mixer to incorporate air and produce volume, often used to create heavy or whipping cream, salad dressings, or sauces"
+    },
+    {
+        label: "Whisk",
+        definition: "a cooking utensil used to blend ingredients in a process such as whipping"
+    },
+    {
+        label: "Yakitori ",
+        definition: "a Japanese dish of small pieces of boneless chicken that is marinated, skewered, and grilled"
+    },
+    {
+        label: "Zest ",
+        definition: "to cut the zest, or the colorful part of the skin that contains oils and provide aroma and flavor, away from the fruit"
     },
 ]
