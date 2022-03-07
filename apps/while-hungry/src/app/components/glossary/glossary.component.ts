8311fcdfe2cfa9bf6d09ad5bab96cabd5c5335f8
Nassim Fernane <nasfernane@gmail.com>
2022-03-07 10:24:15 +0100

    update yml and glossary component

--- apps/while-hungry/src/app/components/glossary/glossary.component.ts
+++ apps/while-hungry/src/app/components/glossary/glossary.component.ts
@@ -9,14 +9,35 @@ import { Definition } from '@prisma/client';
 })
 export class GlossaryComponent implements OnInit {
   glossary: Definition[];
+  definitions: Map<string, any[]>;
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
   constructor(
     private glossaryService: GlossaryService,
-  ) { }
+  ) { 
+    this.definitions = new Map()
+  }
 
   ngOnInit(): void {
     this.glossaryService.getGlossary().subscribe((glossary: Definition[]) => {
-      console.log(glossary);
+      if (glossary) this.glossary = glossary;
+      this.organizeGlossary(this.glossary);
     })
+  }
+
+  organizeGlossary(glossary: Definition[]) {
+    for (const letter of this.alphabet) {
+      const section = [];
+      for (const def of glossary) {
+        if (def.label.toLowerCase().startsWith(letter)) {
+          section.push(def);
+        }
+      }
+
+      this.definitions.set(letter, section)
+    }
+
+    console.log(this.definitions)
   }
 
 }
