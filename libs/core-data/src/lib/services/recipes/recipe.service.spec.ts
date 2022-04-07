c5379c6e8b7accd00c39d0c53cef0bf285c7d6bc
Nassim Fernane <nasfernane@gmail.com>
2022-04-07 10:02:34 +0000

    Revert "test env in data lib"
    This reverts commit 7f8c2e5beea9a89913004ff3431b1604d4c1ef31

---
+++ libs/core-data/src/lib/services/recipes/recipe.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+
+import { RecipeService } from './recipe.service';
+
+describe('RecipeService', () => {
+  let service: RecipeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule]
+    });
+    service = TestBed.inject(RecipeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- libs/core-data/src/lib/services/reviews/review.service.ts
+++ libs/core-data/src/lib/services/reviews/review.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 // utils
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 // schemas
 import { RecipeReview } from '@prisma/client';
