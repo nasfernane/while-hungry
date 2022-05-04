99afcc1a10ae851a31c8b972e9e3e97174bd9e1c
Nassim Fernane <nasfernane@gmail.com>
2022-05-04 19:55:26 +0200

    added bash script to deploy api easily from server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,22 @@
-// config file for back-end deployment using pm2
 module.exports = {
-  apps: [{
-      name: "wh-api",
-      script: "./dist/apps/api/main.js"
-  }]
-}
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/main',
+      repo : 'https://gitlab.com/nasfernane/while-hungry',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
